6b4a83e89cabfe7723d99b50d863b43311a7598d
Cameron Spencer <cameron@lab43.com>
2020-09-08 10:33:03 -0700

    add template helper

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const SVGO = require('svgo');
+const SVGO = require('svgo')
+    , p = require('path')
+;
 
 
 
-module.exports = ({ rule = '**/*.svg' , ...config } = {}) => ({ registerFileHandler }) => {
+module.exports = ({ rule = '**/*.svg' , ...config } = {}) => ({ registerFileHandler, registerTemplateHelper }) => {
 
   const svgo = new SVGO({});
+  const svgs = {};
 
   registerFileHandler(rule, async ({ path, file }) => {
     const result = await svgo.optimize(file);
+    svgs[p.join('/', path)] = result.data;
     return {
       path,
       file: Buffer.from(result.data),
     };
+  });
+
+  registerTemplateHelper('svgo', (metadata, src) => {
+    const path = p.isAbsolute(src)
+      ? src
+      : p.join('/', metadata.dirname, src)
+    ;
+    return svgs[path];
   });
 
 }
